f7124e50b36d0ed54414d912820db7390bdc31b0
Wonseok Jung <54163029+dnjstjr93@users.noreply.github.com>
2020-09-21 18:09:22 +0900

    Add files via upload

--- test.js
+++ test.js
@@ -112,23 +112,23 @@ function runLib(obj_lib) {
             scripts_arr[0] = './' + config.directory_name + '/' + scripts_arr[0];
         }
 
-        var run_lib = spawn(scripts_arr[0], scripts_arr.slice(1));
+        // var run_lib = spawn(scripts_arr[0], scripts_arr.slice(1));
 
-        run_lib.stdout.on('data', function(data) {
-            console.log('stdout: ' + data);
-        });
+        // run_lib.stdout.on('data', function(data) {
+        //     console.log('stdout: ' + data);
+        // });
 
-        run_lib.stderr.on('data', function(data) {
-            console.log('stderr: ' + data);
-        });
+        // run_lib.stderr.on('data', function(data) {
+        //     console.log('stderr: ' + data);
+        // });
 
-        run_lib.on('exit', function(code) {
-            console.log('exit: ' + code);
-        });
+        // run_lib.on('exit', function(code) {
+        //     console.log('exit: ' + code);
+        // });
 
-        run_lib.on('error', function(code) {
-            console.log('error: ' + code);
-        });
+        // run_lib.on('error', function(code) {
+        //     console.log('error: ' + code);
+        // });
     }
     catch (e) {
         console.log(e.message);
